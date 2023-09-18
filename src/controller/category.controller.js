c866cd539b6d2a3662fd312aba28574193acad8d
Dhaval Prajapati <dhavalprajapati01052@gmail.com>
2023-09-18 09:46:24 +0530

    added API for getCategoryWiseSubCategory, addSubSubcategory and getSubCategoryWiseSubSubCategory (category.controller.js)

--- src/controller/category.controller.js
+++ src/controller/category.controller.js
@@ -8,7 +8,7 @@ const {
 	FAILURE,
 	ACTIVE_STATUS,
 } = require("../../config/key");
-const { Category, Subcategory } = require('../models/index');
+const { Category, Subcategory, SubSubcategory } = require('../models/index');
 
 // Add New Contactus
 exports.addCategory = async (req, res) => {
@@ -145,8 +145,6 @@ exports.addSubcategory = async (req, res) => {
         });
 
         const categoryData = category.get({ plain: true });
-        console.log(categoryData, 'categoryData');
-        console.log(reqParam,'reqParam');
 
         if (!category) return responseHelper.error(res, res.__("Category not found with the provided categoryId"), FAILURE);
 
@@ -194,6 +192,135 @@ exports.getSingleSubcategory = async (req, res) => {
         }
     }catch(error){
         // Error Response
+        return responseHelper.error(res, res.__("SomethingWentWrongPleaseTryAgain"), SERVERERROR);
+    }
+}
+
+exports.getCategoryWiseSubCategory = async (req, res) => {
+    try{
+        const categoryId = req.params.id;
+
+        // Validate get category wise subcategory Request.
+        let validationMessage = await categoryValidation.getCategoryWiseSubCategoryValidation({ id: categoryId });
+        if (validationMessage) return responseHelper.error(res, res.__(validationMessage), FAILURE);
+
+        // Find the subcategory by its UUID
+        const category = await Category.findOne({
+            where: {
+                uuid: categoryId
+            },
+        });
+
+        console.log(category);
+
+        if(!category) return responseHelper.error(res, res.__("CategoryNotFound"), FAILURE);
+
+        // Find the subcategory by its UUID
+        const subcategory = await Subcategory.findAll({
+            where: {
+                categoryId: category.id
+            },
+        });
+
+
+        // If the subcategory is found, return it in the response
+        if (subcategory && subcategory.length > 0) {
+            return responseHelper.successapi(res, res.__("Category wise subcategories retrieved successfully"), SUCCESS, subcategory);
+        } else {
+            // If the subcategory is not found, return an appropriate message
+            return responseHelper.error(res, res.__("Subcategory not found"), FAILURE);
+        }
+    }catch(error){
+        // Error Response
+        return responseHelper.error(res, res.__("SomethingWentWrongPleaseTryAgain"), SERVERERROR);
+    }
+}
+
+exports.addSubSubcategory = async (req, res) => {
+    try{
+        // Check if the subcategory table exists and create it if not
+        await Subcategory.sync({ force: false });
+        await SubSubcategory.sync({ force: false });
+
+        const reqParam = req.body;
+
+        // Validate create category Request.
+        let validationMessage = await categoryValidation.createSubSubcategoryValidation(reqParam);
+        if (validationMessage) return responseHelper.error(res, res.__(validationMessage), FAILURE);
+
+        // Check if a subcategory with the same title already exists
+        const existingSubSubcategory = await SubSubcategory.findOne({
+            where: {
+                title: reqParam.title,
+            },
+        });
+
+        if(existingSubSubcategory) return responseHelper.error(res, res.__("SubSubCategory with the same title already exists"), FAILURE);
+
+        const subCategory = await Subcategory.findOne({
+            where: {
+                uuid: reqParam.subCategoryId,
+            },
+            raw: true
+        });
+
+        if (!subCategory) return responseHelper.error(res, res.__("SubCategory not found with the provided subCategoryId"), FAILURE);
+
+        const newSubSubcategory = await SubSubcategory.create({
+            subCategoryId: subCategory?.id,
+            title: reqParam.title,
+            description: reqParam.desc,
+            img: reqParam.img,
+        });
+
+        console.log(newSubSubcategory,'newSubSubcategory');
+
+        // Success Response
+        return responseHelper.successapi(res, res.__("SubSubCategoryCreatedSuccessfully"), SUCCESS, newSubSubcategory);
+    }catch(error){
+        // Error Response
+        console.log(error);
+        return responseHelper.error(res, res.__("SomethingWentWrongPleaseTryAgain"), SERVERERROR);
+    }
+}
+
+exports.getSubCategoryWiseSubSubCategory = async (req, res) => {
+    try{
+        const subCategoryId = req.params.id;
+
+        // Validate get category wise subcategory Request.
+        let validationMessage = await categoryValidation.getSubCategoryWiseSubSubCategoryValidation({ id: subCategoryId });
+        if (validationMessage) return responseHelper.error(res, res.__(validationMessage), FAILURE);
+
+        // Find the subcategory by its UUID
+        const subCategory = await Subcategory.findOne({
+            where: {
+                uuid: subCategoryId
+            },
+        });
+
+        console.log(subCategory);
+
+        if(!subCategory) return responseHelper.error(res, res.__("SubCategoryNotFound"), FAILURE);
+
+        // Find the subcategory by its UUID
+        const subSubCategory = await SubSubcategory.findAll({
+            where: {
+                subCategoryId: subCategory.id
+            },
+        });
+
+
+        // If the subcategory is found, return it in the response
+        if (subSubCategory && subSubCategory.length > 0) {
+            return responseHelper.successapi(res, res.__("SubCategory wise SubSubCategories retrieved successfully"), SUCCESS, subSubCategory);
+        } else {
+            // If the subcategory is not found, return an appropriate message
+            return responseHelper.error(res, res.__("SubSubcategoryNotFound"), FAILURE);
+        }
+    }catch(error){
+        // Error Response
+        console.log(error);
         return responseHelper.error(res, res.__("SomethingWentWrongPleaseTryAgain"), SERVERERROR);
     }
 }
